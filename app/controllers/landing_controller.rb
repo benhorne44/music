c3877d904d21605bd20fe0a96587fb6a136cea5f
Ben Horne <benhorne44@gmail.com>
2014-04-04 20:10:35 -0600

    updates styles and adds songkick api

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,12 +2,27 @@ class LandingController < ApplicationController
 
   def index
     response = Twitter.user_timeline("zheckendorf", count: 3)
-    event_response_raw = Faraday.get("http://api.bandsintown.com/artists/Zach%20Heckendorf/events.json?api_version=2.0&app_id=zach_heckendorf")
-    event_response = JSON.parse(event_response_raw.body)
-
-    # raise event_response.inspect
     @tweet = response.first
     @tweets = response
-    # raise @tweet.inspect
+
+    response = Faraday.get("http://api.songkick.com/api/3.0/artists/2849436/calendar.json?apikey=#{ENV['SONGKICK_API_KEY']}")
+    event_response = JSON.parse(response.body)
+    events = event_response["resultsPage"]["results"]["event"]
+    @events = []
+    events.each do |event|
+      @events << {
+        "venue" => event["venue"],
+        "headliner" => event["performance"].first,
+        "supporting" => event["performance"][1],
+        # look into multiple supporting acts
+        "time" => event["start"]["datetime"],
+        "location" => event["location"],
+        "link" => event["uri"]
+      }
+    end
+    @events = @events[0...6]
+    # @events = events
+    # @events = event_response["resultsPage"]["results"]["event"].first
+    # raise events.inspect
   end
 end
